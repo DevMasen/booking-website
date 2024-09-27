bc5381d4c422c1c788b931d7aecb1415376480b0
DevMasen <mohammadwh400@gmail.com>
2024-09-27 19:43:12 +0330

    feat: responsive the header

--- EghaamatYaar.Web/wwwroot/js/site.js
+++ EghaamatYaar.Web/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿'use strict';
+
+//! HTML ELEMENTS
+const nav = document.querySelector('.nav');
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+//! EVENT HANDLERS
+menuBtn.addEventListener('click', function () {
+  nav.style.right = '0';
+//   document.body.style.height = 'auto';
+});
+
+closeBtn.addEventListener('click', function () {
+  nav.style.right = '-100%';
+//   document.body.style.height = '1440px';
+});
