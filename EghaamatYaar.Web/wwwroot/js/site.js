19813621e790ff1c6163012b9a37a4bf38ed838a
DevMasen <mohammadwh400@gmail.com>
2024-09-27 23:01:54 +0330

    feat: footer structure added

--- EghaamatYaar.Web/wwwroot/js/site.js
+++ EghaamatYaar.Web/wwwroot/js/site.js
@@ -8,10 +8,10 @@ const closeBtn = document.querySelector('.close-btn');
 //! EVENT HANDLERS
 menuBtn.addEventListener('click', function () {
   nav.style.right = '0';
-//   document.body.style.height = 'auto';
+  //   document.body.style.height = 'auto';
 });
 
 closeBtn.addEventListener('click', function () {
   nav.style.right = '-100%';
-//   document.body.style.height = '1440px';
+  //   document.body.style.height = '1440px';
 });
