4c0e8d9586d7d76fce7ba1f00dad40746154b0fe
DevMasen <mohammadwh400@gmail.com>
2024-10-10 20:46:21 +0330

    feat: hide/show feature added for password

--- EghaamatYaar.Web/wwwroot/js/adminSignup.js
+++ EghaamatYaar.Web/wwwroot/js/adminSignup.js
@@ -2,6 +2,8 @@
 
 //! HTML Elements
 const inputs = document.querySelectorAll('.signup-input_input');
+const hideBtn = document.querySelector('.hide-btn');
+const passwordInput = document.getElementById('password');
 
 //! Event Handlers
 inputs.forEach(function (input) {
@@ -13,4 +15,18 @@ inputs.forEach(function (input) {
       input.nextElementSibling.classList.remove('input-focus-fill');
     }
   });
+});
+
+hideBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  const newType =
+    passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  let newIconSrc;
+  if (newType === 'text') {
+    newIconSrc = hideBtn.querySelector('img').src.replace('hide', 'show');
+  } else {
+    newIconSrc = hideBtn.querySelector('img').src.replace('show', 'hide');
+  }
+  hideBtn.querySelector('img').setAttribute('src', newIconSrc);
+  passwordInput.setAttribute('type', newType);
 });
