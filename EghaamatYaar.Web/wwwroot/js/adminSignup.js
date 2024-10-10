10b068b42c2577c68b6a3e34a2fe61862599f0c0
DevMasen <mohammadwh400@gmail.com>
2024-10-10 19:30:15 +0330

    fix: input focus bug fixed and cshtml updated

--- EghaamatYaar.Web/wwwroot/js/adminSignup.js
+++ EghaamatYaar.Web/wwwroot/js/adminSignup.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//! HTML Elements
+const inputs = document.querySelectorAll('.signup-input_input');
+
+//! Event Handlers
+inputs.forEach(function (input) {
+  input.addEventListener('focus', function () {
+    input.nextElementSibling.classList.add('input-focus-fill');
+  });
+  input.addEventListener('blur', function () {
+    if (input.value === '') {
+      input.nextElementSibling.classList.remove('input-focus-fill');
+    }
+  });
+});
